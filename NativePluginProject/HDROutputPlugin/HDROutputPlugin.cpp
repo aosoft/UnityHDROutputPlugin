3efcb782ad0bab4ff2f70a8b486ee6c0b166fb99
aosoft <tan-y@aosoft.jp>
2018-12-06 02:02:58 +0900

    Add Unity Native Plugin functions

--- NativePluginProject/HDROutputPlugin/HDROutputPlugin.cpp
+++ NativePluginProject/HDROutputPlugin/HDROutputPlugin.cpp
@@ -36,3 +36,75 @@ PluginBool HDROutputPlugin::IsAvailableDisplayWindow()
 }
 
 
+template<typename RetT, typename ... Args>
+struct Proxy
+{
+	template<typename RetT(HDROutputPlugin::*func)(Args...)>
+	static RetT UNITY_INTERFACE_API Func(HDROutputPlugin *self, Args... args)
+	{
+		return (self->*func)(args...);
+	}
+};
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+IUnityInterfaces *g_unityInterfaces = nullptr;
+
+int32_t UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API CreateHDROutputPluginInstance(
+	void ** buffer, int32_t bufferSize) try
+{
+	typedef void * void_ptr;
+
+	static const void_ptr funcs[] =
+	{
+		Proxy<void>::Func<&HDROutputPlugin::Destroy>,
+		Proxy<void>::Func<&HDROutputPlugin::CreateDisplayWindow>,
+		Proxy<PluginBool>::Func<&HDROutputPlugin::IsAvailableDisplayWindow>,
+	};
+
+	static constexpr int32_t requiredSize = sizeof(funcs) / sizeof(void *) + 1;
+
+	if (buffer == nullptr || bufferSize < 1)
+	{
+		return requiredSize;
+	}
+
+	if (bufferSize < requiredSize)
+	{
+		return 0;
+	}
+
+	buffer[0] = new HDROutputPlugin();
+	int32_t index = 1;
+	for (auto p : funcs)
+	{
+		buffer[index] = p;
+		index++;
+	}
+
+	return requiredSize;
+}
+catch (const std::exception&)
+{
+	return 0;
+}
+
+void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces* unityInterfaces)
+{
+	g_unityInterfaces = unityInterfaces;
+}
+
+void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
+{
+	g_unityInterfaces = nullptr;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
