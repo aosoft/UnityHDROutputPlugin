24c1261d560a5690adbb8073141bee3a166fb076
aosoft <tan-y@aosoft.jp>
2018-12-30 15:40:21 +0900

    Fix colorspace converter

--- NativePluginProject/HDROutputPlugin/Shader/ps_LinearToSRGB.hlsl
+++ NativePluginProject/HDROutputPlugin/Shader/ps_LinearToSRGB.hlsl
@@ -4,5 +4,5 @@
 float4 main(PS_INPUT input) : SV_Target
 {
 	float4 color = tex.Sample(texSampler, input.uv);
-	return float4(Rec709ToRec2020(LinearToST2084(color.rgb)), 1.0);
+	return float4(LinearToSRGB(color.rgb), 1.0);
 }
